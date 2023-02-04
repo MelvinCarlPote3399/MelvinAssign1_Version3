9c5b845c098131f3253f9dad5d67adb6f9395550
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-02-04 00:54:38 -0500

    - Created the button to go the rental type screen - Created the rent type screen - Modified the application logo (when viewed from outside the application) - Added the French translation page; ensured that the text isn't hard coded - Ensured all the text/strings aren't hard coded - Created the back button to go from the rental screen, back to the home page

--- app/src/main/java/melvincarl/pote/n01483399/PoteActivity.java
+++ app/src/main/java/melvincarl/pote/n01483399/PoteActivity.java
@@ -3,17 +3,32 @@ package melvincarl.pote.n01483399;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityManager;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PoteActivity extends AppCompatActivity {
+
+    private  Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.poteactivity);
-
-
+        button = (Button) findViewById(R.id.enter_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRentActivity();
+            }
+        });
 
     }
+    public void openRentActivity() {
+        Intent intent = new Intent(this, RentActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/melvincarl/pote/n01483399/RentActivity.java
@@ -0,0 +1,18 @@
+package melvincarl.pote.n01483399;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+}
