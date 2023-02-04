c7cf5792a2d03de02ff7bb89b937500cc9847117
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-02-04 18:41:42 -0500

    -Added toasts to when an option is clicked on the rent screen; certain toast message will appear depending on the user choice -Created the visit page screen

--- app/src/main/java/melvincarl/pote/n01483399/RentActivity.java
+++ app/src/main/java/melvincarl/pote/n01483399/RentActivity.java
@@ -1,10 +1,21 @@
 package melvincarl.pote.n01483399;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class RentActivity extends AppCompatActivity {
+
+    //creating a variable of type image button
+    ImageButton rentImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +25,45 @@ public class RentActivity extends AppCompatActivity {
         //Back button code
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //Radio group and its radio buttons
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.HomeTypeGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton rb1 = (RadioButton) findViewById(R.id.Apartment);
+                RadioButton rb2 = (RadioButton) findViewById(R.id.Detached_home);
+                if(rb1.isChecked()){
+                    DisplayToast();
+                }
+                else{
+                    DisplayToast2();
+                }
+
+            }
+        });
+
+        ImageButton rentImageButton;
+
+        rentImageButton = (ImageButton) findViewById(R.id.RentButton);
+        rentImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToVisitScreen = new Intent(RentActivity.this,VisitScreen.class);
+                startActivity(goToVisitScreen);
+            }
+        });
 
     }
+
+    public void DisplayToast() {
+        Toast.makeText(RentActivity.this,"Apartment selected!",Toast.LENGTH_LONG).show();
+    }
+
+    public void DisplayToast2(){
+        Toast.makeText(RentActivity.this,"Detached home selected!",Toast.LENGTH_LONG).show();
+    }
+
+
+
+
 }
