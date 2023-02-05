645d6a4d5d899b4dabe5f20276c62aacd49b91c2
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-02-05 04:04:15 -0500

    -Page for Detached home completed -Images added -Checkboxes added -Program will not go to payment page, until user selects one option (minimum); similar to our Apartment page screen

--- app/src/main/java/melvincarl/pote/n01483399/DetachedHomeActivity.java
+++ app/src/main/java/melvincarl/pote/n01483399/DetachedHomeActivity.java
@@ -2,16 +2,72 @@ package melvincarl.pote.n01483399;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class DetachedHomeActivity extends AppCompatActivity {
+
+    ImageButton detachedhomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detached_home);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        CheckBox house1 = (CheckBox) findViewById(R.id.houseBox1);
+        CheckBox house2 = (CheckBox) findViewById(R.id.houseBox2);
+        CheckBox house3 = (CheckBox) findViewById(R.id.houseBox3);
+
+        house1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View h1) {
+                if(((CheckBox)h1).isChecked()){
+                    Toast.makeText(DetachedHomeActivity.this, "House 1 selected", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        house2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View h2) {
+                if(((CheckBox)h2).isChecked()){
+                    Toast.makeText(DetachedHomeActivity.this,"House 2 selected",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        house3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View h3) {
+                if(((CheckBox)h3).isChecked()){
+                    Toast.makeText(DetachedHomeActivity.this,"House 3 selected",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        detachedhomeButton = (ImageButton) findViewById(R.id.ProceedPayment2);
+        detachedhomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View detachHome) {
+                if (!house1.isChecked() && !house2.isChecked() && !house3.isChecked()){
+                    Toast.makeText(DetachedHomeActivity.this,"Please select an option!",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Intent goToPayment2 = new Intent(DetachedHomeActivity.this,PaymentActivity.class);
+                    startActivity(goToPayment2);
+                }
+            }
+        });
+
+
 
     }
 }
