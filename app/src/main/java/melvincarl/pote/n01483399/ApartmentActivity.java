3eb7b1ad3144776c4589af39ff86328313f27dc3
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-02-05 01:15:29 -0500

    -Added content to Apartment page; added images w/ descriptions/addresses of properties -Created checkboxes; requirements asked for a minimum of 3 -Still have yet to add 'Payment' button

--- app/src/main/java/melvincarl/pote/n01483399/ApartmentActivity.java
+++ app/src/main/java/melvincarl/pote/n01483399/ApartmentActivity.java
@@ -9,6 +9,9 @@ public class ApartmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_visit_screen);
+        setContentView(R.layout.activity_apartment_screen);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
